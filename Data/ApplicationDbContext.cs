3246be9f2af8606b6adb03ed7ac6343a853e7edd
Best-Verie <irabestverie2016@gmail.com>
2022-08-25 09:56:53 +0200

    feat: database migration files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ntuma.Models;
+
+namespace Ntuma.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace Ntuma.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ntuma.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrdeer { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
